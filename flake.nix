7992041932d3ce1e9379e65c6835bb261af51fb4
Otavio Salvador <otavio@ossystems.com.br>
2022-03-21 19:29:12 -0300

    nix: convert to use flake.nix
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "rsget";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-21.11";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            pkg-config
+            openssl
+          ];
+        };
+      });
+}
