a3cf767cff45087b40f2e75e7ca7701ef773524b
Otavio Salvador <otavio@ossystems.com.br>
2024-10-22 10:59:31 -0300

    nix: Use flake-parts
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- flake.nix
+++ flake.nix
@@ -1,24 +1,57 @@
 {
-  description = "rsget";
-
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-24.05";
-    flake-utils = {
-      url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+
+    nix-github-actions = {
+      url = "github:nix-community/nix-github-actions";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    rust-flake = {
+      url = "github:juspay/rust-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShell = pkgs.mkShell {
+  outputs = inputs@{ flake-parts, ... }:
+    let
+      inherit (inputs.nixpkgs) lib;
+      inherit (inputs) self;
+    in
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.rust-flake.flakeModules.default
+        inputs.rust-flake.flakeModules.nixpkgs
+      ];
+
+      systems = lib.systems.flakeExposed;
+
+      flake.githubActions = inputs.nix-github-actions.lib.mkGithubMatrix {
+        checks = lib.getAttrs [ "x86_64-linux" ] self.checks;
+      };
+
+      perSystem = { self', config, pkgs, ... }: {
+        packages.default = self'.packages.rsget;
+
+        rust-project.crates.rsget.crane.args = {
           buildInputs = with pkgs; [
             pkg-config
             openssl
+          ];
+        };
+
+
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [
+            self'.packages.rsget
+          ];
+
+          packages = with pkgs; [
             cargo
             rustc
             rustfmt
@@ -26,5 +59,6 @@
             rust-analyzer
           ];
         };
-      });
+      };
+    };
 }
